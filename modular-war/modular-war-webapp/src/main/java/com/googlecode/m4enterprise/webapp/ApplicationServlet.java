5ff413c6bb6fc7f4dddf9a4110648b96abda3661
Marcin.Kuthan <marcin.kuthan@gmail.com>
2011-01-17 15:37:21 +0000

    Add jetty plugin definition. Change web apps to be J2EE 1.4 compatible.

--- modular-war/modular-war-webapp/src/main/java/com/googlecode/m4enterprise/webapp/ApplicationServlet.java
+++ modular-war/modular-war-webapp/src/main/java/com/googlecode/m4enterprise/webapp/ApplicationServlet.java
@@ -5,15 +5,12 @@ import java.io.PrintWriter;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebInitParam;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.googlecode.m4enterprise.core.ApplicationService;
 
-@WebServlet(name = "applicationServlet", urlPatterns = { "/webapp" }, initParams = { @WebInitParam(name = "name", value = "Web Application") })
 public class ApplicationServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
