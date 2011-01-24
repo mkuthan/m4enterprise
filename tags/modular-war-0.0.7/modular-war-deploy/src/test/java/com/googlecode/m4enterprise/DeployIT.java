34fdff1f2824a0b1ad3a6fb0a1daf224d5b262bb
Marcin.Kuthan <Marcin.Kuthan@b77907a0-1c6b-a5aa-efda-816b357c4bdd>
2011-01-24 11:25:04 +0000

    [maven-release-plugin]  copy for tag modular-war-0.0.7

---
+++ tags/modular-war-0.0.7/modular-war-deploy/src/test/java/com/googlecode/m4enterprise/DeployIT.java
@@ -0,0 +1,14 @@
+package com.googlecode.m4enterprise;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class DeployIT {
+
+    @Test
+    public void smokeDeploymentTest() {
+	assertTrue("Define real smoke deployment test", true);
+    }
+
+}
